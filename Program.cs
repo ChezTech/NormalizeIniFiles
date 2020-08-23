68bd35e159bba273826220b355267f5b878295b0
Joshua Gage <josh@chezgage.com>
2020-08-22 21:51:03 -0700

    Start project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NormalizeIniFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+        }
+
+        private bool ValidateArgs(string[] args)
+        {
+            return false;
+        }
+    }
+}
