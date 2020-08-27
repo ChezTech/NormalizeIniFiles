1bb011c920c194991dc38b61fa36233568a30249
Joshua Gage <josh@chezgage.com>
2020-08-26 22:07:47 -0700

    Simple script that will call NormalizeIniFiles for each EQ character

---
+++ NormalizeAllChars.ps1
@@ -0,0 +1,30 @@
+﻿# Normalize all character .ini files
+
+$AllCharNames = @(
+"aaaaa",
+"bbbbb",
+"ccccc"
+)
+
+$BaseEQFolder = "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest"
+$NormalizeExecutablePath = ".\NormalizeIniFiles.exe"
+
+function NormalizeFile()
+{
+    param ($FilePath)
+    Start-Process -FilePath $NormalizeExecutablePath -ArgumentList $FilePath -WindowStyle Hidden
+    #-NoNewWindow # This doesn't work though it seems like it should be the better option instead of hiding the window
+}
+
+function NormalizeAllFilesForName()
+{
+    param ($CharName)
+
+
+    NormalizeFile( Join-Path -Path $BaseEQFolder -ChildPath ($CharName + "_eqclient.ini"))
+    NormalizeFile( Join-Path -Path $BaseEQFolder -ChildPath ($CharName + "_test.ini"))
+    NormalizeFile( Join-Path -Path $BaseEQFolder -ChildPath ("UI_" + $CharName + "_test.ini"))
+}
+
+
+$AllCharNames | ForEach-Object {NormalizeAllFilesForName($_)}
