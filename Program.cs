95d12a97849701926a760add4577f94a818b00ff
Joshua Gage <josh@chezgage.com>
2021-01-02 23:56:19 -0800

    Add support for file globbing, aka wildcard searching for files.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
@@ -24,7 +25,9 @@ namespace NormalizeIniFiles
             // Tab completion: https://github.com/dotnet/command-line-api/blob/main/docs/dotnet-suggest.md
 
             // Example args:
-            // --file "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\Aiyya_test.ini" --file "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\UI_Aiyya_test.ini"
+            // --file "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\Aiyya_test.ini" --file "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\UI_Aiyya_test.ini" --dryRun
+            // --baseDir "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest" --pattern "*_eqclient.ini" "*_test.ini" "*_erollisi.ini"
+            // --file "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\Aiyya_test.ini" --file "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\UI_Aiyya_test.ini" --baseDir "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest" --pattern "UI_*_test.ini" --pattern "*_eqclient.ini" "*_test.ini" "*_erollisi.ini" --dryRun
 
             var rootCommand = new RootCommand
             {
@@ -32,16 +35,28 @@ namespace NormalizeIniFiles
                 {
                     Description = "The name of the .ini file(s) to normalize. May specify more than one.",
                     Argument = new Argument<IEnumerable<FileInfo>>().ExistingOnly(),
-                    IsRequired = true,
+                },
+                new Option<DirectoryInfo>( new[] {"--baseDir", "-d"})
+                {
+                    Description = "Base directory from which to do the glob pattern match.",
+                    Argument = new Argument<DirectoryInfo>().ExistingOnly(),
+                },
+                new Option<IEnumerable<string>>( new[] {"--pattern", "-p"})
+                {
+                    Description = "The glob pattern to use in the base directory for the pattern match",
+                },
+                new Option<bool>( new[] {"--dryRun"})
+                {
+                    Description = "DryRun will just list the files to be normalized without actually doing it.",
                 },
             };
 
             rootCommand.Description = @"Sort the keys and options (normalize) for a .ini file(s)
 This is useful for consistent comparision when checking into source control.";
 
-            rootCommand.Handler = CommandHandler.Create<IEnumerable<FileInfo>>((file) =>
+            rootCommand.Handler = CommandHandler.Create<IEnumerable<FileInfo>, DirectoryInfo, IEnumerable<string>, bool>((file, baseDir, pattern, dryRun) =>
             {
-                return new Program().MainHandler(file);
+                return new Program().MainHandler(file, baseDir, pattern, dryRun);
             });
 
             // Parse the incoming args and invoke the handler
@@ -75,16 +90,53 @@ This is useful for consistent comparision when checking into source control.";
             return args;
         }
 
-        private int MainHandler(IEnumerable<FileInfo> files)
+        private int MainHandler(IEnumerable<FileInfo> files, DirectoryInfo baseDir, IEnumerable<string> patterns, bool dryRun)
         {
-            Parallel.ForEach(files, file =>
+            var filePaths = GetFileList(files, baseDir, patterns);
+
+            Parallel.ForEach(filePaths, file =>
             {
-                var normalizer = new Normalizer();
-                normalizer.NormalizeFile(file.FullName);
-                Console.WriteLine($"File has been normalized: {file.FullName}");
+                if (dryRun)
+                {
+                    Console.WriteLine($"File would be normalized: {file}");
+                }
+                else
+                {
+                    var normalizer = new Normalizer();
+                    normalizer.NormalizeFile(file);
+                    Console.WriteLine($"File has been normalized: {file}");
+                }
             });
 
+            if (filePaths.Count() == 0)
+                Console.WriteLine("No files found to normalize. Use the '--help' option to see usage.");
+            else
+                Console.WriteLine($"Total of {filePaths.Count()} files");
+
             return 0;
+        }
+
+        /// <summary>
+        /// Based on the CLI args, find the matching files. Return just a distinct set in case there are duplicates matches
+        /// </summary>
+        private IEnumerable<string> GetFileList(IEnumerable<FileInfo> files, DirectoryInfo baseDir, IEnumerable<string> patterns)
+        {
+            var filePaths = files?.Select(x => x.FullName) ?? Array.Empty<string>();
+
+            if (baseDir != null && patterns != null)
+            {
+
+                var matcher = new Matcher();
+                matcher.AddIncludePatterns(patterns);
+                var matchedFiles = matcher.GetResultsInFullPath(baseDir.FullName);
+
+                // Union() removes duplicates from the return set, so we don't need to 'Distinct()' the collection ourselves.
+                filePaths = filePaths.Union(matchedFiles);
+            }
+
+            // We still need to sort it though
+            // (still need to Distinct, since user can pass in duplicate files via the '--file' arg)
+            return filePaths.Distinct().OrderBy(x => x);
         }
     }
 }
